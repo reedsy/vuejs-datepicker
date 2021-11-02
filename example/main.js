844d4ca40dbfb999a33d1bc934b376cc882df45a
Victor Borràs <vborrasc@gmail.com>
2021-11-02 16:54:11 +0100

    ⬆️ Update to Vue 3

--- example/main.js
+++ example/main.js
@@ -1,10 +1,4 @@
-import Vue from 'vue';
-import DatepickerDemo from './DatepickerDemo.vue';
+import { createApp } from 'vue'
+import DatepickerDemo from './DatepickerDemo.vue'
 
-Vue.config.productionTip = false;
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(DatepickerDemo),
-});
+createApp(DatepickerDemo).mount('#app')
